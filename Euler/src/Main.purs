1ec851805436d193d5a0fbae892ec84929950b84
Michał Kowalk <mich.kowalik@gmail.com>
2021-04-30 11:24:43 +0200

    Add more cases with descriptions.

--- Euler/src/Main.purs
+++ Euler/src/Main.purs
@@ -10,6 +10,10 @@ import Effect.Console (log)
 
 main :: Effect Unit
 main = do
+  -- pow 10 4 - stack exceeded
+  log $ show $ calculationsProduct toNumber productFrom2 (pow 10 3)
+  -- pow 10 8 - memory exceeded
+  log $ show $ productFrom2 (pow 10 7)
   -- the target is to compute G(10^8) % (10^9 + 7)
   log $ show $ calculationsProduct toNumber gaussFactorial 18
   -- higher numbers exceed the IEEE float32 range of 2^127
